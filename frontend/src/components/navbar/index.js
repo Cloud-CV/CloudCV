87402288368d2799a98442fe8ff9e9e26f5dc4ba
Utkarsh Gupta <utkarsh357@gmail.com>
2017-06-02 21:29:03 +0530

    feat(navbar): Make navbar responsive

--- frontend/src/components/navbar/index.js
+++ frontend/src/components/navbar/index.js
@@ -1,37 +1,63 @@
 import React, { Component } from "react";
 import { PropTypes } from "prop-types";
 import { Link, withRouter } from "react-router-dom";
-import { connect } from "react-redux";
 import NavbarItem from "./NavbarItem";
 
-const Navbar = props => {
-  let listItems = ["", "Projects", "News", "GSoC", "Team", "Contribute"];
-  listItems = listItems.map((path, index) => {
-    let active = `/${path.toLowerCase()}` === props.location.pathname;
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  render() {
+    let listItems = ["", "Projects", "News", "GSoC", "Team", "Contribute"];
+    listItems = listItems.map((path, index) => {
+      let active = `/${path.toLowerCase()}` === this.props.location.pathname;
+      return (
+        <NavbarItem active={active} key={path}>
+          <Link to={`\/${path.toLowerCase()}`}>
+            {path === "" ? "Home" : path}
+          </Link>
+        </NavbarItem>
+      );
+    });
+    let listHiddenClass = this.state.isOpen ? "" : "hidden-small";
+    let listIconClass = this.state.isOpen
+      ? "cv-navbar-close-icon"
+      : "cv-navbar-list-icon";
+
     return (
-      <NavbarItem active={active} key={path}>
-        <Link to={`\/${path.toLowerCase()}`}>
-          {path === "" ? "Home" : path}
-        </Link>
-      </NavbarItem>
+      <nav className="cv-navbar-container">
+        <div className={`cv-navbar-list-container ${listHiddenClass}`}>
+          <ul className="cv-navbar-list">
+            {listItems}
+          </ul>
+        </div>
+        <div className="cv-mobile-topbar">
+          <div className="cv-navbar-brand">
+            <img
+              className="cv-navbar-brand-image"
+              src={require("images/cloudcv_logo.png")}
+            />
+          </div>
+          <div
+            className={`cv-icon ${listIconClass}`}
+            onClick={this.handleClick}
+          />
+        </div>
+        {this.state.isOpen &&
+          <div className="cv-navbar-overlay" onClick={this.handleClick} />}
+      </nav>
     );
-  });
-  return (
-    <nav className="cv-navbar-container">
-      <div className="cv-navbar-list-container">
-        <ul className="cv-navbar-list">
-          {listItems}
-        </ul>
-      </div>
-      <div className="cv-navbar-brand">
-        <img
-          className="cv-navbar-brand-image"
-          src={require("images/cloudcv_logo.png")}
-        />
-      </div>
-    </nav>
-  );
-};
+  }
+}
 
 Navbar.propTypes = {
   match: PropTypes.object.isRequired,
@@ -39,8 +65,4 @@ Navbar.propTypes = {
   history: PropTypes.object.isRequired
 };
 
-function mapStateToProps(state, ownProps) {
-  return {};
-}
-
-export default withRouter(connect(mapStateToProps)(Navbar));
+export default withRouter(Navbar);
