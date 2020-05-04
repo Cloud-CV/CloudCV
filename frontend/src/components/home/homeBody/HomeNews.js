548d785870fd7f85ecb20aa2faebece6d015271c
Gautam Jajoo <gowmsigautam2002@gmail.com>
2020-05-04 21:03:55 +0530

    Add GSOC 2020

--- frontend/src/components/home/homeBody/HomeNews.js
+++ frontend/src/components/home/homeBody/HomeNews.js
@@ -3,8 +3,22 @@ import { Link } from "react-router-dom";
 import TimelineCard from "../../common/TimelineCard";
 import Button from "../../common/Button";
 
-const MIN_ELEMENTS = 17;
+const MIN_ELEMENTS = 18;
 const EVENTS = [
+  {
+    date: "February 2020",
+    desc: (
+      <span>
+        CloudCV selected as a mentoring organisation for{" "}
+        <Link
+          to="https://summerofcode.withgoogle.com/organizations/6054438164234240/"
+          target="blank"
+        >
+          GSOC 2020
+        </Link>
+      </span>
+    )
+  },
   {
     date: "November 2019",
     desc: (
@@ -24,11 +38,7 @@ const EVENTS = [
     desc: (
       <span>
         EvalAI and Fabrik are accepted to{" "}
-        <Link
-          to="https://sosp19.rcs.uwaterloo.ca/"
-          target="blank"
-          rel="noopener noreferrer"
-        >
+        <Link to="https://sosp19.rcs.uwaterloo.ca/" target="blank">
           SOSP Conference 2019
         </Link>, Canada
       </span>
@@ -43,7 +53,6 @@ const EVENTS = [
         <Link
           to="https://summerofcode.withgoogle.com/organizations/5709446018236416/"
           target="blank"
-          rel="noopener noreferrer"
         >
           GSOC 2019
         </Link>
@@ -59,7 +68,6 @@ const EVENTS = [
         <Link
           to="https://codein.withgoogle.com/organizations/cloudcv/"
           target="blank"
-          rel="noopener noreferrer"
         >
           GCI 2018
         </Link>
@@ -75,7 +83,6 @@ const EVENTS = [
         <Link
           to="https://summerofcode.withgoogle.com/organizations/4737237502132224/"
           target="blank"
-          rel="noopener noreferrer"
         >
           GSOC 2018
         </Link>
@@ -90,7 +97,6 @@ const EVENTS = [
         <Link
           to="https://codein.withgoogle.com/archive/2017/organization/5692767623708672/"
           target="blank"
-          rel="noopener noreferrer"
         >
           GCI 2017
         </Link>
@@ -102,11 +108,7 @@ const EVENTS = [
     desc: (
       <span>
         CloudCV selected for demo presentation at{" "}
-        <Link
-          to="http://cvpr2017.thecvf.com/"
-          target="blank"
-          rel="noopener noreferrer"
-        >
+        <Link to="http://cvpr2017.thecvf.com/" target="blank">
           CVPR 2017
         </Link>
         , Hawaii
@@ -118,11 +120,7 @@ const EVENTS = [
     desc: (
       <span>
         CloudCV selected as a semifinalist at{" "}
-        <Link
-          to="http://www.ldv.co/visionsummit/"
-          target="blank"
-          rel="noopener noreferrer"
-        >
+        <Link to="http://www.ldv.co/visionsummit/" target="blank">
           LDV Vision Summit 2017
         </Link>
         , New York City
@@ -137,7 +135,6 @@ const EVENTS = [
         <Link
           to="https://summerofcode.withgoogle.com/organizations/5427409970003968/"
           target="blank"
-          rel="noopener noreferrer"
         >
           GSoC 2017
         </Link>
@@ -149,11 +146,7 @@ const EVENTS = [
     desc: (
       <span>
         CloudCV selected for demo presentation at{" "}
-        <Link
-          to="http://cvpr2016.thecvf.com/program/demos"
-          target="blank"
-          rel="noopener noreferrer"
-        >
+        <Link to="http://cvpr2016.thecvf.com/program/demos" target="blank">
           CVPR 2016
         </Link>
         , Las Vegas, NV
@@ -168,7 +161,6 @@ const EVENTS = [
         <Link
           to="https://summerofcode.withgoogle.com/archive/2016/organizations/4785685563179008/"
           target="blank"
-          rel="noopener noreferrer"
         >
           GSoC 2016
         </Link>
@@ -183,7 +175,6 @@ const EVENTS = [
         <Link
           to="https://www.google-melange.com/archive/gsoc/2015/orgs/cloudcv"
           target="blank"
-          rel="noopener noreferrer"
         >
           GSoC 2015
         </Link>
