37b2c8df9b9b6d50742fc800f1ec848bfcf42b00
Cypre55 <satwik.chappidi@iitkgp.ac.in>
2020-12-01 21:17:07 +0530

    Corrected a typo in "About Us" section

--- frontend/src/components/home/homeBody/HomeAboutUs.js
+++ frontend/src/components/home/homeBody/HomeAboutUs.js
@@ -5,7 +5,7 @@ import Card from "../../common/Card";
 const HomeAboutUs = props => {
   const ABOUT_US_TITLE = "WHO ARE WE";
   const ABOUT_US_DESC =
-    "CloudCV is a young open source cloud platform started in 2013 by students and faculty from Machine Learning and Perception Lab at Virginia Tech (now at Georgia Tech) with the aim to make AI research more reproducible. At CloudCV, we are building tools that enable researchers to build, compare and share start-of-the-algorithms. We believe that one shouldn't have to be an AI expert to have access to cutting edge vision algorithms. Likewise, researchers shouldn't have to worry about building a service around their deep learning models to showcase and share it with others.";
+    "CloudCV is a young open source cloud platform started in 2013 by students and faculty from Machine Learning and Perception Lab at Virginia Tech (now at Georgia Tech) with the aim to make AI research more reproducible. At CloudCV, we are building tools that enable researchers to build, compare and share state-of-the-art algorithms. We believe that one shouldn't have to be an AI expert to have access to cutting edge vision algorithms. Likewise, researchers shouldn't have to worry about building a service around their deep learning models to showcase and share it with others.";
   return (
     <div className="cv-home-about-us cv-container" id="about">
       <div className="cv-home-card-container">
